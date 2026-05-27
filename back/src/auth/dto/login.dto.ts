b99ce6e43101c6d4f5a619b4b28fc0ff5187f575
amiriseif <seifeddine.amiri@insat.ucar.tn>
2026-05-27 18:20:47 +0100

    Refactor LoginDto and RegisterDto for consistent formatting

--- back/src/auth/dto/login.dto.ts
+++ back/src/auth/dto/login.dto.ts
@@ -1,12 +1,12 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
-    @IsNotEmpty()
-    @IsEmail()
-    email!: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
 
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(6)
-    password!: string;
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password!: string;
 }
