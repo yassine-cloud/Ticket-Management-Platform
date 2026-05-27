144df088729f115c3b4911373f2cf45fd4fd8284
amiriseif <seifeddine.amiri@insat.ucar.tn>
2026-05-27 06:32:59 +0100

    add DatabaseService to AppController and enable CORS in main.ts

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: DatabaseService,
+  ) {}
 
   @Get()
   getHello(): string {
